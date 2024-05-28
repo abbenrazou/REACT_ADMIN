f7269eae46d6a6a5c07c8955a0a0161a5961adf8
abbenrazou <137790477+abbenrazou@users.noreply.github.com>
2024-05-28 11:03:04 +0200

    Add files via upload

---
+++ ra-app/src/components/PosterList.tsx
@@ -0,0 +1,40 @@
+import { Datagrid, ImageField, List, NumberField, ReferenceField, TextField, useRecordContext ,EditButton
+    ,TopToolbar,SelectColumnsButton,ExportButton,DatagridConfigurable,
+} from 'react-admin';
+import { PosterFilterSidebar } from './FilterList';
+
+const PosterListActions=()=>(
+    <TopToolbar>
+        <SelectColumnsButton/>
+        <ExportButton/>
+    </TopToolbar>
+);
+
+const UrlField = ({source}:{source:string})=>{
+    const record = useRecordContext()
+    if(!record) return null
+    return <a href={record[source]} target="_blank">View</a>;
+}
+
+export const PosterList = () => (
+    <List aside={<PosterFilterSidebar/>} actions={<PosterListActions/>}>
+        <DatagridConfigurable >
+            <TextField source="id" />
+            <ReferenceField source="category_id" reference="categories">
+                <TextField source='name'></TextField>
+                </ReferenceField>
+            <TextField source="title" />
+            <NumberField source="width" label="width(cm)"/>
+            <NumberField source="height" label="height(cm)" />
+            <NumberField source="price" />
+            <ImageField source="thumbnail" title="title" sx={{
+                "& img":{maxWidth:80,maxHeight:80,objectFit:"contain"},
+            }}/>
+            <UrlField source="image" />
+            <TextField source="description" />
+            <NumberField source="stock" />
+            <NumberField source="sales" sx={{fontWeight: "bold"}}/>
+            <EditButton/>
+        </DatagridConfigurable>
+    </List>
+);
